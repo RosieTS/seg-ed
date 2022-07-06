ee83462ec3901d47b963c255d05df4d262d5f971
Rosie Telford Spencer <ret58@cam.ac.uk>
2022-07-06 10:18:53 +0100

    Re-added missing function in extract_patches

--- images/extract_patches.py
+++ images/extract_patches.py
@@ -2,11 +2,11 @@
 Script to import directory of WSIs and corresponding epithelium masks and to output these
 as overlapping patches.
 Patches with > 75% background pixels currently discarded.
-TO DO: WSI and mask directories, and file names are currently hard-coded. Make these a command line option.
 TO DO: Patch size and stride also hard-coded. Make these a command-line option.
 '''
 
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import openslide
@@ -16,20 +16,43 @@ from skimage.color import rgb2gray
 from skimage.filters import threshold_otsu as otsu
 
 
+def get_args(name = 'default', level_dim = 2, 
+    WSI_dir="/home/rosie/epithelium_slides/WSIs",
+    mask_dir="/home/rosie/epithelium_slides/export_openslide",
+    output_dir="/home/rosie/epithelium_slides/output_images"):
+    
+    return int(level_dim), WSI_dir, mask_dir, output_dir
  
 
+def get_image_and_mask_names(WSI_dir, mask_dir):
+ 
+    files = sorted(os.listdir(WSI_dir))
 
-def get_patch_origin_coords(image, patch_size, stride):
+    image_file_names = []
+    mask_file_names = []
+
+    for file in files:
+        image_file_names.append(os.path.join(WSI_dir, file))
+        mask_file_names.append(os.path.join(mask_dir, file[0:-4] + "_epithelium_mask.tif"))
+
+    # Won't work if only 1 file in each dir.
+    print(image_file_names)
+    print(mask_file_names)
+
+    return image_file_names, mask_file_names
+
+
+def get_patch_origin_coords(image, patch_size, stride, level_dim):
     # Need to make sure taking the same dimensions from image and mask.
-    # This function assumes we're taking patches from level_dimensions[2]
-
+    
     # Image dimensions at highest resolution:
     width, height = image.level_dimensions[0]
 
     # Starting coords are in coords of full sized image
     # The -1 stops us from outputting patches that overlap with edge of image
-    x_coords = [x * stride[0] * 4 for x in range(0,(width // (stride[0]*4) - 1))]
-    y_coords = [y * stride[1] * 4 for y in range(0,(height // (stride[1]*4) - 1))]
+
+    x_coords = [x * stride[0]  * 2**level_dim for x in range(0,(width // (stride[0] * 2**level_dim) - 1))]
+    y_coords = [y * stride[1]  * 2**level_dim for y in range(0,(height // (stride[1] * 2**level_dim) - 1))]
 
     return x_coords, y_coords
 
@@ -60,15 +83,15 @@ def find_background_fraction(extracted_patch, threshold):
     return background_fraction
 
 
-def extract_patch(image, x, y, patch_size = (224, 224)):
+def extract_patch(image, x, y, level_dim, patch_size = (224, 224)):
 
-    extracted_patch = image.read_region((x,y), 2, patch_size, ).convert('RGB')
+    extracted_patch = image.read_region((x,y), level_dim, patch_size, ).convert('RGB')
       
     return extracted_patch
 
-def extract_mask(mask, x, y, patch_size = (224, 224)):
+def extract_mask(mask, x, y, level_dim, patch_size = (224, 224)):
    
-    patch = mask.read_region((x,y), 2, patch_size, ).convert('L')
+    patch = mask.read_region((x,y), level_dim, patch_size, ).convert('L')
     patch_array = np.asarray(patch).astype('uint8')*255
     extracted_mask = Image.fromarray(patch_array)
 
@@ -88,16 +111,18 @@ if __name__ == "__main__":
     #WSI_dir="/mnt/c/Users/rosie/WSL/helpful_code/data-loader/WSIs"
     #mask_dir="/mnt/c/Users/rosie/WSL/helpful_code/data-loader/masks"
     #output_dir="/mnt/c/Users/rosie/WSL/output_images"
-    WSI_dir="/home/rosie/epithelium_slides/WSIs"
-    mask_dir="/home/rosie/epithelium_slides/export_openslide"
-    output_dir="/home/rosie/epithelium_slides/output_images"
+#    WSI_dir="/home/rosie/epithelium_slides/WSIs"
+#    mask_dir="/home/rosie/epithelium_slides/export_openslide"
+#    output_dir="/home/rosie/epithelium_slides/output_images"
+
+    level_dim, WSI_dir, mask_dir, output_dir = get_args(*sys.argv)
+    print(level_dim)
+    print(WSI_dir)
+    print(mask_dir)
+    print(output_dir)
 
     file_names = get_image_and_mask_names(WSI_dir, mask_dir)
     tol = 0.75
-#    print(file_names)
-#    print(type(file_names))
-#    print(len(file_names))
-#    print(mask_file_names)
 
     for image_file_name, mask_file_name in file_names:
         print(image_file_name)
@@ -106,14 +131,14 @@ if __name__ == "__main__":
         mask = openslide.OpenSlide(mask_file_name)
         patch_size = (284, 284)
         stride = (142, 142)
-
+    
         #print(image.level_dimensions)
         #print(mask.level_dimensions)
  
         threshold =  get_otsu_threshold(image)
         print(threshold)
 
-        x_coords, y_coords = get_patch_origin_coords(image, patch_size, stride)
+        x_coords, y_coords = get_patch_origin_coords(image, patch_size, stride, level_dim)
         #print(x_coords)
         #print(y_coords)
 
@@ -121,11 +146,11 @@ if __name__ == "__main__":
             for y in y_coords:
                 #if y == 16576 and x == 16576:
 
-                extracted_patch = extract_patch(image, x, y, patch_size = patch_size)
+                extracted_patch = extract_patch(image, x, y, level_dim, patch_size = patch_size)
                 background_fraction = find_background_fraction(extracted_patch, threshold)
 
                 if background_fraction <= tol:
                     save_patch(extracted_patch, output_dir, image_file_name)
 
-                    extracted_mask = extract_mask(mask, x, y, patch_size = patch_size)
+                    extracted_mask = extract_mask(mask, x, y, level_dim, patch_size = patch_size)
                     save_patch(extracted_mask, output_dir, mask_file_name)
